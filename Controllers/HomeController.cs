243009b3a7a0084777cafd1ea264b6d3be61e751
geetaingle <geetaingle321@gmail.com>
2023-09-24 01:43:48 +0530

    Sending get request to controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieBuffs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new
+            {
+                title = "",
+                rating = 0.0
+            };
+            return Json(data);
+        }
+    }
+}
