293ee029003dcca51aad1dc4659ba02d4c124c2f
geetaingle <geetaingle321@gmail.com>
2023-09-24 00:56:36 +0530

    Add: Homepage and List UI

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,36 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 let listTypes = [
   {
     id: 0,
     Title: "Movies",
-    cardColor: "red",
+    cardColor: 'rgb(255, 161, 114)',
   },
   {
     id: 1,
     Title: "Series",
-    cardColor: "green",
+    cardColor: "rgb(163, 255, 179)",
   },
   {
     id: 2,
     Title: "Latest Releases",
-    cardColor: "blue",
+    cardColor: "rgb(198, 224, 255)",
   }
 ]
 
-export class Home extends Component {
-  static displayName = Home.name;
+export function Home() {
 
-  render() {
-    return (
-      <div>
-        <div><>Trending</></div>
-        <div>{
-          listTypes.map(item => <div key={item.id}>
-            <button style={{ background: item.cardColor }}>{item.Title}</button>
+  const navigate = useNavigate();
+
+  const getList = (id) => navigate('/list?' + id)
+
+  return (
+    <div className='home'>
+      <div><button className="trending">Trending</button></div>
+      <div>{
+        listTypes.map(item =>
+          <div className='card'
+            key={item.id}
+            style={{ background: item.cardColor }}
+            onClick={() => getList(item.id)}
+          >
+            {item.Title}
           </div>)
-        }</div>
-      </div>
-    );
-  }
+      }</div>
+    </div>
+  )
 }
--- ClientApp/src/components/List.js
+++ ClientApp/src/components/List.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function List() {
+
+    const [resData, setResData] = useState(null);
+
+    const url = "http://hi.com";
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+
+                setResData(response.data);
+            }
+            catch (e) {
+                console.error('Error fetching data: ', e);
+            }
+        }
+
+        fetchData();
+
+    }, [])
+
     return (
         <div>
-            <>This is List</>
+            <h1>Movies</h1>
+            {resData ? (
+                <pre>{JSON.stringify(resData, null, 2)}</pre>
+            ) : (<p>Loading...</p>)}
         </div>
     )
 }
